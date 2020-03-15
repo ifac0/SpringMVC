10f673091d8de7d8d87c25165acb6cf783243e15
Ivan Costa <ivan.costa@justa.com.vc>
2020-03-15 00:33:53 -0300

    feat: :sparkles: Criaçao do projeto para estudo com SprignBoot e MongoDb; Entity User; Instanciando a base de dados; Usando DTO; FindById; Tratamento de exception;
    Signed-off-by: Ivan Costa <ivan.costa@justa.com.vc>

---
+++ workshopmongo/src/main/java/com/ifac0/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.ifac0.workshopmongo.repository;
+
+import com.ifac0.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ workshopmongo/src/main/java/com/ifac0/workshopmongo/services/UserService.java
@@ -0,0 +1,26 @@
+package com.ifac0.workshopmongo.services;
+
+import com.ifac0.workshopmongo.domain.User;
+import com.ifac0.workshopmongo.repository.UserRepository;
+import com.ifac0.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
