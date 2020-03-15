6405999c6a313ca9d637d65679bf2717448a1872
Ivan Costa <ivan.costa@justa.com.vc>
2020-03-15 00:50:24 -0300

    feat: :construction: add POST;
    Signed-off-by: Ivan Costa <ivan.costa@justa.com.vc>

--- workshopmongo/src/main/java/com/ifac0/workshopmongo/services/UserService.java
+++ workshopmongo/src/main/java/com/ifac0/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.ifac0.workshopmongo.services;
 
 import com.ifac0.workshopmongo.domain.User;
+import com.ifac0.workshopmongo.dto.UserDTO;
 import com.ifac0.workshopmongo.repository.UserRepository;
 import com.ifac0.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return  repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
