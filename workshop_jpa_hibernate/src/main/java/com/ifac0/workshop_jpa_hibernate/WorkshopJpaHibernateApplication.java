0df44de34052019623cdcd437fad0c94b1abfe2a
Ivan Costa <ivan.costa@justa.com.vc>
2020-03-16 00:24:42 -0300

    feat: Altera nome do diretorio; Cria projeto do spring com JPA e Hibernate
    Signed-off-by: Ivan Costa <ivan.costa@justa.com.vc>

---
+++ workshop_jpa_hibernate/src/main/java/com/ifac0/workshop_jpa_hibernate/WorkshopJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.ifac0.workshop_jpa_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopJpaHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopJpaHibernateApplication.class, args);
+    }
+
+}
---
+++ workshop_jpa_hibernate/src/test/java/com/ifac0/workshop_jpa_hibernate/WorkshopJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifac0.workshop_jpa_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopJpaHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
