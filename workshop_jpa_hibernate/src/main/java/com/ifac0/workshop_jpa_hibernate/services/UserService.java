59a928f99b43608513ebe5b6f93f149e333158c8
Ivan Costa <ivan.costa@justa.com.vc>
2020-03-17 22:48:09 -0300

    feat: :art: Exception;
    Signed-off-by: Ivan Costa <ivan.costa@justa.com.vc>

--- workshop_jpa_hibernate/src/main/java/com/ifac0/workshop_jpa_hibernate/services/UserService.java
+++ workshop_jpa_hibernate/src/main/java/com/ifac0/workshop_jpa_hibernate/services/UserService.java
@@ -2,9 +2,14 @@ package com.ifac0.workshop_jpa_hibernate.services;
 
 import com.ifac0.workshop_jpa_hibernate.entities.User;
 import com.ifac0.workshop_jpa_hibernate.repositories.UserRepository;
+import com.ifac0.workshop_jpa_hibernate.services.exceptions.DatabaseException;
+import com.ifac0.workshop_jpa_hibernate.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,7 +25,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
@@ -28,13 +33,22 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
-    }
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }    }
 
     public User update(Long id, User obj) {
-        User entity = repository.getOne(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getOne(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
---
+++ workshop_jpa_hibernate/src/main/java/com/ifac0/workshop_jpa_hibernate/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.ifac0.workshop_jpa_hibernate.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+}
