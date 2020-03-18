3b4e3e54e1473388dc09a0feacc7d87e87d7a8df
Ivan Costa <ivan.costa@justa.com.vc>
2020-03-17 22:35:17 -0300

    feat: :art: Subtotal, total e payment;
    Signed-off-by: Ivan Costa <ivan.costa@justa.com.vc>

--- workshop_jpa_hibernate/src/main/java/com/ifac0/workshop_jpa_hibernate/entities/Order.java
+++ workshop_jpa_hibernate/src/main/java/com/ifac0/workshop_jpa_hibernate/entities/Order.java
@@ -31,6 +31,9 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order(){}
 
     public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
@@ -81,8 +84,24 @@ public class Order implements Serializable {
         return Objects.equals(id, order.id);
     }
 
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+
     public Set<OrderItem> getItems() {
         return items;
+    }
+
+    public Double getTotal() {
+        double sum = 0.0;
+        for (OrderItem x : items) {
+            sum += x.getSubTotal();
+        }
+        return sum;
     }
 
     @Override
